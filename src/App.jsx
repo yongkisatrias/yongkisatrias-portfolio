86cda4c2443d1565d6b2505cbcad9d87d00b60f4
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-12-23 02:34:35 +0700

    [UPDATE] Create navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import Test from "./Test";
 import "./app.scss";
+
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <section>Hero</section>
-      <section>Parallax</section>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
       <section>Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
+      {/* <Test />
+      <Test /> */}
     </div>
   );
 };
