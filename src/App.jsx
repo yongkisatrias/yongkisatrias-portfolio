5d28bac21071bf3d40a13855d2c2319a720a5f73
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-12-26 15:24:19 +0700

    [UPDATE] Create Parallax components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import Test from "./Test";
 import "./app.scss";
 import Hero from "./components/hero/Hero";
-
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return (
@@ -11,9 +11,13 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Services</section>
-      <section>Parallax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
